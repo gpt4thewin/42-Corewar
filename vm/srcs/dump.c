5239f275904c6d0fcec147874d40696e6c4d2070
Adam GOULAS <agoulas@e3r5p5.42.fr>
2019-03-14 19:22:17 +0100

    dump fonction ajouter

--- vm/srcs/dump.c
+++ vm/srcs/dump.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   dump.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
+/*   By: agoulas <agoulas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 17:44:00 by juazouz           #+#    #+#             */
-/*   Updated: 2019/03/14 17:44:21 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/03/14 19:21:21 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,21 @@
 // Taille de MEM_SIZE
 void			dump(char *memory)
 {
+	int i;
 
+	i = 0;
+	ft_printf("0x%04x : ", 0);
+	while(i < MEM_SIZE)
+	{
+		ft_printf("%02.2hhx", memory[i]);
+		if (((i + 1) % 64 != 0))
+			ft_printf(" ");
+		else
+		{
+			ft_printf("\n");
+			if (i < (MEM_SIZE - 1))
+				ft_printf("0x%04x : ", i + 1);
+		}
+		i++;
+	}
 }
