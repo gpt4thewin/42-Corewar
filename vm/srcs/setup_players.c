212679d74de656e8a6c17a5b0570fe94b6cde167
Adam GOULAS <agoulas@e2r2p6.42.fr>
2019-03-19 17:56:45 +0100

    modification ft_memcpy_color(t_corewar *corewar, t_player *player, size_t num,size_t pos) :affichage initial en couleur fonctionne.

--- vm/srcs/setup_players.c
+++ vm/srcs/setup_players.c
@@ -6,25 +6,28 @@
 /*   By: agoulas <agoulas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 13:50:54 by juazouz           #+#    #+#             */
-/*   Updated: 2019/03/19 15:08:15 by agoulas          ###   ########.fr       */
+/*   Updated: 2019/03/19 17:55:37 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	*ft_memcpy_color(t_corewar *corewar, const void *src, size_t num, int color)
+void	*ft_memcpy_color(t_corewar *corewar, t_player *player, size_t num,
+	size_t pos)
 {
-	size_t i;
-	void *dst;
+	size_t	i;
+	void	*dst;
+	void	*src;
 
 	dst = corewar->memory;
+	src = &player->program;
 	if (dst == src)
 		return (dst);
 	i = 0;
 	while (i < num)
 	{
-		((unsigned char*)dst)[i] = ((unsigned char*)src)[i];
-		corewar->id_memory[i] = color;
+		((unsigned char*)dst)[pos + i] = ((unsigned char*)src)[i];
+		corewar->id_memory[pos + i] = player->color;
 		i++;
 	}
 	return (dst);
@@ -34,7 +37,7 @@ void	setup_player_at_pos(t_corewar *corewar, t_player *player, size_t pos)
 {
 	t_process	*entry_process;
 
-	ft_memcpy_color(corewar, &player->program, CHAMP_MAX_SIZE, player->id);
+	ft_memcpy_color(corewar, player, CHAMP_MAX_SIZE, pos);
 	corewar->id_memory[pos] = player->id;
 	entry_process = process_new();
 	entry_process->pc = pos;
