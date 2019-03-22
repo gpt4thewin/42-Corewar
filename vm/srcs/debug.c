c419645c67d86f233ecb2b19a2416fa32f83fcea
Adam GOULAS <agoulas@e1r4p1.42.fr>
2019-03-22 18:41:03 +0100

    norme debug.c

--- vm/srcs/debug.c
+++ vm/srcs/debug.c
@@ -6,35 +6,34 @@
 /*   By: agoulas <agoulas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 16:24:47 by agoulas           #+#    #+#             */
-/*   Updated: 2019/03/21 19:21:57 by agoulas          ###   ########.fr       */
+/*   Updated: 2019/03/22 18:39:42 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-
-void debug_player(t_player player)
+void	debug_player(t_player player)
 {
-	ft_fprintf(2, "id = %d,name :%s \n",player.id, player.prog_name);
-	ft_fprintf(2,"color = %d, file = %s\n", player.color, player.file);
-	ft_fprintf(2,"last_check = %d\n", player.last_check);
+	ft_fprintf(2, "id = %d,name :%s \n", player.id, player.prog_name);
+	ft_fprintf(2, "color = %d, file = %s\n", player.color, player.file);
+	ft_fprintf(2, "last_check = %d\n", player.last_check);
 }
 
-
-void debug_process(t_process *p)
+void	debug_process(t_process *p)
 {
 	int i;
 
 	i = 0;
-	ft_fprintf(2,"\n___________________________________________________\n");
+	ft_fprintf(2, "\n___________________________________________________\n");
 	debug_player(*p->player);
-	ft_fprintf(2, "pc = %d, next_cycle :%d,nbr_live = %d \n",p->pc, p->next_cycle,p->nbr_live);
+	ft_fprintf(2, "pc = %d, next_cycle :%d,nbr_live = %d \n", p->pc,
+		p->next_cycle, p->nbr_live);
 	while (i < REG_NUMBER)
 	{
-		ft_fprintf(2,"reg[%d] = %2.2hhx \n",i, p->reg[i]);
-		if (i == ((REG_NUMBER -1)/ 2))
-			ft_fprintf(2,"\n");
+		ft_fprintf(2, "reg[%d] = %2.2hhx \n", i, p->reg[i]);
+		if (i == ((REG_NUMBER - 1) / 2))
+			ft_fprintf(2, "\n");
 		i++;
 	}
-	ft_fprintf(2,"\n___________________________________________________\n");
+	ft_fprintf(2, "\n___________________________________________________\n");
 }
