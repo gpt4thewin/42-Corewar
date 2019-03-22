6af9035e86fa32790aeae160b5ef712e90398c81
Adam GOULAS <agoulas@e1r4p1.42.fr>
2019-03-22 19:21:22 +0100

    norme parse_parameter_id.c

--- vm/srcs/parse_parameters_id.c
+++ vm/srcs/parse_parameters_id.c
@@ -6,11 +6,15 @@
 /*   By: agoulas <agoulas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/22 18:43:40 by agoulas           #+#    #+#             */
-/*   Updated: 2019/03/22 18:58:39 by agoulas          ###   ########.fr       */
+/*   Updated: 2019/03/22 19:20:52 by agoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+/*
+**	REturn if a id is already used.
+*/
 
 static int		id_has_duplicate(t_corewar *corewar, int id)
 {
@@ -28,22 +32,25 @@ static int		id_has_duplicate(t_corewar *corewar, int id)
 	return (nb);
 }
 
-static void player_has_id(t_corewar *corewar,int cpt)
+static void		player_has_id(t_corewar *corewar, int cpt)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (corewar->players[cpt].id == -1 && i < corewar->players_count)
 	{
 		if (corewar->players[cpt].id == -1
-			&& id_has_duplicate(corewar, i) == 0)
+			&& !id_has_duplicate(corewar, i))
 			corewar->players[cpt].id = i;
-			i++;
+		i++;
 	}
 	if (id_has_duplicate(corewar, corewar->players[cpt].id) > 1)
 		corewar_die("Error player : duplicate id");
 }
 
+/*
+**	Test every player and give a id to player without id.
+*/
 
 void			players_init_id(t_corewar *corewar)
 {
