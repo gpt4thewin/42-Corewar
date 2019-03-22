116b5d370bf4dba8e2c3e040ae5570f550a96dfd
Julien AZOUZ <juazouz@e1r4p3.42.fr>
2019-03-22 15:46:21 +0100

    vm: rework de load_params() avec correctif pour zjmp

--- vm/srcs/load_params.c
+++ vm/srcs/load_params.c
@@ -6,7 +6,7 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/20 15:56:51 by juazouz           #+#    #+#             */
-/*   Updated: 2019/03/21 16:13:35 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/03/22 15:38:20 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,33 +17,29 @@ static t_bool	type_allowed(t_op *op, t_arg_type type, int index)
 	return (type != 0 && op->args[index] & (0x1 << (type - 1)));
 }
 
-static t_bool	load_param(t_arg_type type, size_t *pos, void *ptr,
-	t_paramval *val)
+static t_bool	load_param(t_param *param, size_t *pos, void *ptr)
 {
-	t_paramval			res;
-
-	if (type == REG_CODE)
+	if (param->type == REG_CODE)
 	{
-		res.reg_id = *((char*)ptr + (*pos));
-		if (!is_valid_reg(res.reg_id))
+		param->value.reg_id = *((char*)ptr + (*pos));
+		if (!is_valid_reg(param->value.reg_id))
 			return (false);
 		(*pos) += sizeof(char);
 	}
-	else if (type == DIR_CODE)
+	else if (param->type == DIR_CODE)
 	{
-		res.dir = *((t_dir*)ptr + (*pos));
-		res.dir = convert_endian32(res.dir);
+		param->value.dir = *((t_dir*)ptr + (*pos));
+		param->value.dir = convert_endian32(param->value.dir);
 		(*pos) += sizeof(t_dir);
 	}
-	else if (type == IND_CODE || type == DIR_MOD_CODE)
+	else if (param->type == IND_CODE || param->type == DIR_MOD_CODE)
 	{
-		ft_memcpy(&res.ind, ptr + (*pos), sizeof(t_ind));
-		res.ind = convert_endian16(res.ind);
+		ft_memcpy(&param->value.ind, ptr + (*pos), sizeof(t_ind));
+		param->value.ind = convert_endian16(param->value.ind);
 		(*pos) += sizeof(t_ind);
 	}
 	else
 		return (false);
-	*val = res;
 	return (true);
 }
 
@@ -55,27 +51,29 @@ t_bool			load_params(t_op *op, t_instruction *inst, t_paraminfo *paraminfo)
 	struct s_inst_param	*multi;
 	t_arg_type			type;
 
+	pos = 0;
 	ft_bzero(paraminfo, sizeof(t_paraminfo));
 	if (!op->has_argcode)
 	{
 		paraminfo->args_number = 1;
-		paraminfo->params[0].type = DIR_CODE;
-		paraminfo->params[0].value.dir = inst->param.single_dir;
+		if (op->has_size_mod)
+			paraminfo->params[0].type = DIR_MOD_CODE;
+		else
+			paraminfo->params[0].type = DIR_CODE;
+		load_param(&paraminfo->params[0], &pos, &inst->param.single_dir);
 		return (true);
 	}
 	multi = &inst->param.multi;
 	types = *((char*)&inst->param.multi);
 	i = 0;
-	pos = 0;
 	while (i < op->args_number)
 	{
-		type = ((types << (2 * i)) & 0xc0) >> 6;
-		if (!type_allowed(op, type, i))
+		paraminfo->params[i].type = ((types << (2 * i)) & 0xc0) >> 6;
+		if (!type_allowed(op, paraminfo->params[i].type, i))
 			return (false);
-		if (op->has_size_mod && type == DIR_CODE)
-			type = DIR_MOD_CODE;
-		paraminfo->params[i].type = type;
-		if (!load_param(type, &pos, &multi->parameters, &paraminfo->params[i].value))
+		if (op->has_size_mod && paraminfo->params[i].type == DIR_CODE)
+			paraminfo->params[i].type = DIR_MOD_CODE;
+		if (!load_param(&paraminfo->params[i], &pos, &multi->parameters))
 			return (false);
 		paraminfo->args_number++;
 		i++;
