4056db25d3ab529afc072b0880a11bc65d329550
Julien AZOUZ <juazouz@e3r11p1.42.fr>
2019-03-28 15:56:58 +0100

    .........

--- vm/srcs/output.c
+++ vm/srcs/output.c
@@ -6,21 +6,18 @@
 /*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 13:35:04 by agoulas           #+#    #+#             */
-/*   Updated: 2019/03/28 15:53:54 by juazouz          ###   ########.fr       */
+/*   Updated: 2019/03/28 15:57:35 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-static void	print_player(t_corewar *corewar, int pos)
+static void	print_player(t_player *player)
 {
-	t_player player;
-
-	player = corewar->players[pos];
-	ft_printf("* Player %d,", player.id);
-	ft_printf(" weighing %d bytes,", player.prog_size);
-	ft_printf(" \"%s\",", player.prog_name);
-	ft_printf(" (\"%s\").", player.comment);
+	ft_printf("* Player %d,", player->id);
+	ft_printf(" weighing %d bytes,", player->prog_size);
+	ft_printf(" \"%s\",", player->prog_name);
+	ft_printf(" (\"%s\").", player->comment);
 	ft_printf("\n");
 }
 
@@ -32,7 +29,7 @@ void		print_start_fight(t_corewar *corewar)
 	ft_printf("Introducing contestants...\n");
 	while (index < corewar->players_count)
 	{
-		print_player(corewar, index);
+		print_player(&corewar->players[index]);
 		index++;
 	}
 }
