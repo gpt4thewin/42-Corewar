fd4fbe4e56f2b983e9ecc7ad3c724d5a4748ffbf
Julien AZOUZ <juazouz@e1r5p2.42.fr>
2019-03-29 14:25:37 +0100

    libft: safe_malloc()................

--- libft/srcs/memory/ft_memalloc.c
+++ libft/srcs/memory/ft_memalloc.c
@@ -3,28 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agoulas <agoulas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/16 18:29:07 by juazouz           #+#    #+#             */
-/*   Updated: 2019/03/28 18:41:26 by agoulas          ###   ########.fr       */
+/*   Updated: 2019/03/29 14:21:25 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static ft_safe_malloc(size_t size)
+static void	*ft_safe_malloc(size_t size)
 {
 	void	*ptr;
 
 	ptr = malloc(size);
 	if (ptr == NULL)
 	{
-		ft_printf("COREWAR: ERROR MALLOC\n");
+		ft_fprintf(2, "ERROR MALLOC\n");
 		exit(EXIT_FAILURE);
 	}
+	return (ptr);
 }
 
-void	*ft_memalloc(size_t size)
+void		*ft_memalloc(size_t size)
 {
 	void	*ptr;
 
