8caee84904381b6e6277213712d24a390b0084e6
Julien AZOUZ <juazouz@e2r2p11.42.fr>
2019-04-01 15:49:27 +0200

    Norme

--- asm/srcs/lexer/is_indirect_label.c
+++ asm/srcs/lexer/is_indirect_label.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_indirect_label.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbakhti <mbakhti@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 02:49:48 by mbakhti           #+#    #+#             */
-/*   Updated: 2019/03/08 19:14:20 by mbakhti          ###   ########.fr       */
+/*   Updated: 2019/04/01 15:48:49 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ static _Bool	check_lexical_error(char *tok, t_pos cursor)
 	{
 		if (!ft_strchr(LABEL_CHARS, tok[i]))
 		{
-			ft_fprintf(2, "Lexical error at [%d:%d]\n", cursor.lin, cursor.col + i);
+			ft_fprintf(2, "Lexical error at [%d:%d]\n", cursor.lin,
+				cursor.col + i);
 			exit(EXIT_FAILURE);
 		}
 		i++;
