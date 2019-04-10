cb8ea33b2532961f73e8404240fd65351c742fae
Julien AZOUZ <juazouz@e3r11p8.42.fr>
2019-04-10 15:06:13 +0200

    leak asm/gnl cas special

--- asm/srcs/lexer/is_command.c
+++ asm/srcs/lexer/is_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_command.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbakhti <mbakhti@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 02:49:48 by mbakhti           #+#    #+#             */
-/*   Updated: 2019/03/08 19:14:22 by mbakhti          ###   ########.fr       */
+/*   Updated: 2019/04/10 15:16:04 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ _Bool	is_command(char *tok, char next, t_pos cursor)
 	if (ft_strchr(" \"\t\n\0", next))
 	{
 		if (ft_strequ(NAME_CMD_STRING, tok)
-		|| ft_strequ(COMMENT_CMD_STRING, tok))
+			|| ft_strequ(COMMENT_CMD_STRING, tok))
 			return (true);
 	}
 	return (false);
--- asm/srcs/lexer/is_register.c
+++ asm/srcs/lexer/is_register.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_register.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mbakhti <mbakhti@student.42.fr>            +#+  +:+       +#+        */
+/*   By: juazouz <juazouz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/02 02:49:48 by mbakhti           #+#    #+#             */
-/*   Updated: 2019/03/08 19:14:17 by mbakhti          ###   ########.fr       */
+/*   Updated: 2019/04/10 15:16:00 by juazouz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ _Bool	is_register(char *tok, char next, t_pos cursor)
 	size_t	i;
 
 	if (!ft_isdigit(next) || next == SEPARATOR_CHAR
-	|| ft_strchr(" \t\n\0", next))
+		|| ft_strchr(" \t\n\0", next))
 	{
 		if (ft_strlen(tok) > 1 && tok[0] == 'r' && ft_strlen(tok) < 4)
 		{
